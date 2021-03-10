3e7216f73dab1cf67b3a31e4530447e43430b3de
Artem Dybin <tema.dybin@yandex.ru>
2021-03-10 14:11:11 +0300

    Add files via upload

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './less/styles.less';
+
+styles;
+window.addEventListener('load', () => {
+  ReactDOM.render(<Widget />, document.getElementById('react_root'));
+})
